ef37a4e3b80ac3c696b4efd10c5335fbef0c2e95
DenisNovac <ponydnk@gmail.com>
2020-01-21 20:12:04 +0500

    - #14 Добавлен тест PhoneEntrySuit, использующий PM на полученном JSON; - Удалён второй тест, в котором больше нет смысла, ведь менеджментом листов телефонных номеров будет заниматься PhoneBookHandler.

--- src/test/scala/phonebook/PhoneEntrySuit.scala
+++ src/test/scala/phonebook/PhoneEntrySuit.scala
@@ -1,54 +1,24 @@
 package phonebook
 
 import org.junit._
-import org.junit.Assert.assertEquals
 import io.circe.syntax._
-import io.circe.Json
-
 import phonebook.PhoneEntryHandler.PhoneEntry
-
+import phonebook.PhoneEntryHandler.decoder
 import java.util.UUID.randomUUID
-import java.util.UUID.fromString
 
 class PhoneEntrySuit {
 
   @Test def `Test entry created by "asJson"`: Unit = {
-    val uuid = fromString("7affcd26-6181-49fd-9c3b-10166b4bdfbe")
+    val Name = "Josh"
+    val Num = "1"
+    val entry = PhoneEntry(randomUUID(), Name, Num).asJson
 
-    val sampleEntry =
-      s"""{
-        |  "id" : "$uuid",
-        |  "name" : "Josh",
-        |  "phoneNumber" : "1"
-        |}""".stripMargin
+    val decoded = decoder.decodeJson(entry)
 
-    val entry = PhoneEntry(uuid, "Josh", "1").asJson
-    assertEquals(sampleEntry, entry.toString)
+    decoded match {
+      case Right(x) => x match { case PhoneEntry(_, Name, Num) => true }
+      case Left(e) => false
+    }
   }
 
-
-  @Test def `Test list of entries created by "asJson"`: Unit = {
-    val uuid1 = fromString("7affcd26-6181-49fd-9c3b-10166b4bdfbe")
-    val uuid2 = fromString("b74b907b-a742-4eee-a893-af5991fb1dff")
-    val sampleList =
-      s"""[
-        |  {
-        |    "id" : "$uuid1",
-        |    "name" : "Josh",
-        |    "phoneNumber" : "1"
-        |  },
-        |  {
-        |    "id" : "$uuid2",
-        |    "name" : "Sam",
-        |    "phoneNumber" : "2"
-        |  }
-        |]""".stripMargin
-
-
-    val entry1 = PhoneEntry(uuid1, "Josh", "1")
-    val entry2 = PhoneEntry(uuid2, "Sam", "2")
-    val book = List[PhoneEntry](entry1, entry2)
-
-    assertEquals(sampleList, book.asJson.toString)
-  }
 }
