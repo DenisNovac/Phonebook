039221251739b74498bec9616735c2ab4e35ea29
DenisNovac <ponydnk@gmail.com>
2020-01-24 22:30:37 +0500

    #32 - Добавить поддержку CORS В рамках задачи добавлена обёртка CORS для всего API. Теперь Swagger успешно отправляет запросы.

--- src/main/scala/www/Phonebook.scala
+++ src/main/scala/www/Phonebook.scala
@@ -1,7 +1,29 @@
 package www
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect._
+import cats.implicits._
+import org.http4s.server.Router
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.implicits._
+import Api._
+import cats.data.Kleisli
+import org.http4s.{Request, Response}
 
 object Phonebook extends IOApp{
-  override def run(args: List[String]): IO[ExitCode] = ???
+
+  val port = 9000
+  val host = "172.18.1.2"
+
+  val calls = indexCall <+> addContact
+  val apiCalls: Kleisli[IO, Request[IO], Response[IO]] = Router("/" -> calls).orNotFound
+  val wrapper = CorsApiWrapper(apiCalls)
+
+  override def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(port, host)
+      .withHttpApp(wrapper)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
