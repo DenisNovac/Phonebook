69ef827c9af6794ba159e864b7e54d496926a263
DenisNovac <ponydnk@gmail.com>
2020-01-25 21:41:51 +0500

    #39 - В Compose вписан Swagger UI - Добавлен роут /api, отдающий конфигурацию - Docker build копирует конфигурацию в контейнер - Swagger Editor закомментирован в compose

--- src/main/scala/www/PhoneBook.scala
+++ src/main/scala/www/PhoneBook.scala
@@ -2,11 +2,12 @@ package www
 
 import cats.effect._
 import cats.implicits._
+
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
 import Api._
-
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object PhoneBook extends IOApp{
 
@@ -15,11 +16,12 @@ object PhoneBook extends IOApp{
   //val host = "localhost"
 
   val calls = indexCall <+> addContact <+> listContacts <+> findContactsByName <+> findContactsByPhone <+>
-    getContactById <+> updateContact <+> deleteContact
-
+    getContactById <+> updateContact <+> deleteContact <+> apiCall
   val apiCalls = Router("/" -> calls).orNotFound
   val wrapper = CorsApiWrapper(apiCalls)
 
+  //override implicit val timer: Timer[IO] = IO.timer(global)
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
   override def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
       .bindHttp(port, host)
