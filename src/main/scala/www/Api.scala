5e03ebc1cad2421afaedb9b844f46526091c11f3
DenisNovac <ponydnk@gmail.com>
2020-01-25 20:30:27 +0500

    #36 #29 Запросы пользуются IO-обёрткой - Написал IO-обёртку HttpIoBookHandler для работы с PhoneBookHandler в IO-монаде - Написал методы API, требуемые в предоставленной спецификации

--- src/main/scala/www/Api.scala
+++ src/main/scala/www/Api.scala
@@ -28,5 +28,49 @@ object Api {
       } yield resp
   }
 
+  val listContacts = HttpRoutes.of[IO] {
+    case GET -> Root / "contacts" =>
+      for {
+        resp <- listContactsIo()
+      } yield resp
+  }
+
+  object NameParser extends QueryParamDecoderMatcher[String]("name")  // достаёт имена из куска запроса
+  val findContactsByName = HttpRoutes.of[IO] {
+    case  GET -> Root / "contact" / "findByName" :? NameParser(name) =>
+      for {
+        resp <- findContactByNameIo(name.split(",").toList)
+      } yield resp
+  }
+
+  object PhoneParser extends QueryParamDecoderMatcher[String]("phone")  // достаёт номера из куска запроса
+  val findContactsByPhone = HttpRoutes.of[IO] {
+    case  GET -> Root / "contact" / "findByPhone" :? PhoneParser(phone) =>
+      for {
+        resp <- findContactByPhoneIo(phone.split(",").toList)
+      } yield resp
+  }
+
+  val getContactById = HttpRoutes.of[IO] {
+    case GET -> Root / "contact" / contactId =>
+      for {
+        resp <- getContactByIdIo(contactId.toLong)
+      } yield resp
+  }
+
+  val updateContact = HttpRoutes.of[IO] {
+    case req @ PUT -> Root / "contact" / contactId =>
+      for {
+        contactRequest <- req.as[ContactRequest]
+        resp <- updateContactIo(contactId.toLong, contactRequest)
+      } yield resp
+  }
+
+  val deleteContact = HttpRoutes.of[IO] {
+    case DELETE -> Root / "contact" / contactId =>
+      for {
+        resp <- deleteContactIo(contactId.toLong)
+      } yield resp
+  }
 
 }
